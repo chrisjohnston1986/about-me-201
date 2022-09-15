f10de8ed8c74094339320e25e731138112b290ee
Chris Johnston <chrisjohnston1986@yahoo.com>
2022-09-14 22:15:02 -0500

    TA help

--- js/app.js
+++ js/app.js
@@ -14,56 +14,56 @@ alert(`Welcome to my site, ${visitorName}! Please guess yes or no to the followi
 
 let questionOneGuess = prompt('Am I original??').toLowerCase();
 
-console.log('QuestionOne >>> ', questionOneGuess);
+console.log('QuestionOneGuess >>> ', questionOneGuess);
 
-if(questionOneGuess === 'y' || questionOneGuess === 'yes'){
+if (questionOneGuess === 'y' || questionOneGuess === 'yes') {
   alert('Yeaaaaaaaaah!');
   score++;
-} else if(questionOneGuess === 'n' || questionOneGuess === 'no'){
+} else if (questionOneGuess === 'n' || questionOneGuess === 'no') {
   alert('Wrong!');
 }
 
 let questionTwoGuess = prompt('Am I the only one??').toLowerCase();
 
-console.log('QuestionTwo >>> ', questionTwoGuess);
+console.log('QuestionTwoGuess >>> ', questionTwoGuess);
 
-if(questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
+if (questionTwoGuess === 'y' || questionTwoGuess === 'yes') {
   alert('Yeaaaaaaaaah!');
   score++;
-} else if(questionTwoGuess === 'n' || questionTwoGuess === 'no'){
+} else if (questionTwoGuess === 'n' || questionTwoGuess === 'no') {
   alert('Wrong!');
 }
 
 let questionThreeGuess = prompt('Am I ??seggsual??').toLowerCase();
 
-console.log('QuestionThree >>> ', questionThreeGuess);
+console.log('QuestionThreeGuess >>> ', questionThreeGuess);
 
-if(questionThreeGuess === 'y' || questionThreeGuess === 'yes'){
+if (questionThreeGuess === 'y' || questionThreeGuess === 'yes') {
   alert('Yeaaaaaaaaah!');
   score++;
-} else if(questionThreeGuess === 'n' || questionThreeGuess === 'no'){
+} else if (questionThreeGuess === 'n' || questionThreeGuess === 'no') {
   alert('Wrong!');
 }
 
 let questionFourGuess = prompt('Am I everything you need??').toLowerCase();
 
-console.log('QuestionFour >>> ', questionFourGuess);
+console.log('QuestionFourGuess >>> ', questionFourGuess);
 
-if(questionFourGuess === 'y' || questionFourGuess === 'yes'){
+if (questionFourGuess === 'y' || questionFourGuess === 'yes') {
   alert('YOU BETTER ROCK YOUR BODY NOW!!!');
   score++;
-} else if(questionFourGuess === 'n' || questionFourGuess === 'no'){
+} else if (questionFourGuess === 'n' || questionFourGuess === 'no') {
   alert('Wrong!');
 }
 
 let questionFiveGuess = prompt('Are you done with this game??').toLowerCase();
 
-console.log('QuestionFive >>> ', questionFiveGuess);
+console.log('QuestionFiveGuess >>> ', questionFiveGuess);
 
-if(questionFiveGuess === 'y' || questionFiveGuess === 'yes'){
+if (questionFiveGuess === 'y' || questionFiveGuess === 'yes') {
   alert('ALRIGHT!!!');
   score++;
-} else if(questionFiveGuess === 'n' || questionFiveGuess === 'no'){
+} else if (questionFiveGuess === 'n' || questionFiveGuess === 'no') {
   alert('Wrong! ........just messing with you.');
 }
 
@@ -71,23 +71,76 @@ let myNum = 7;
 
 let guessNum = 4;
 
-while(guessNum>0){
+while (guessNum > 0) {
   let userNumGuess = prompt('Guess what number, < 10, I am thinking about?');
   guessNum--;
-  console.log('NumberGuess >>> ', userNumGuess);
-  if (parseInt(userNumGuess) === myNum){
-    alert ('Whoa!!! You nailed it. How did you do that???');
+  console.log('NumberSixGuess >>> ', userNumGuess);
+  if (parseInt(userNumGuess) === myNum) {
+    alert('Whoa!!! You nailed it. How did you do that???');
     score++;
-    guessNum=0;
+    guessNum = 0;
   }
-  else if(parseInt(userNumGuess) > myNum ){
-    alert ('Nope... too high - try again!');
+  else if (parseInt(userNumGuess) > myNum) {
+    alert('Nope... too high - try again!');
   }
-  else if(parseInt(userNumGuess) < myNum ){
-    alert ('Nope... too low - try again!');
+  else if (parseInt(userNumGuess) < myNum) {
+    alert('Nope... too low - try again!');
   }
 }
 
+// let franchiseArr = ['Star Wars', 'Marvel', 'DC', 'Dragonball Z', 'Game of Thrones', 'Harry Potter', 'Lord of the Rings', 'Pokemon', 'Final Fantasy', 'Star Trek'];
+// let correctAnswer = false;
+// let guessNumSeven = 6;
+
+// for (let i = 0; i < guessNumSeven; i++) {
+//   let userNumSevenGuess = prompt('Guess one of my favorite cinematic universes?').toLowerCase();
+//   // guessNumSeven-=1;
+//   console.log('NumberSevenGuess >>> ', userNumSevenGuess);
+//   for (let j = 0; j < franchiseArr.length; j++) {
+//     if (userNumSevenGuess === franchiseArr[j].toLowerCase()) {
+//       alert('ONE OF MY FAVORITES!');
+//       // eslint-disable-next-line no-unused-vars
+//       correctAnswer = true;
+//       score++;
+//     }
+//   }
+//   if(correctAnswer === true) {
+//     break;
+//   }
+// }
+//   if(i === guessNumSeven || correctAnswer) {
+//   alert(`Garfield helped me. His favorite franchises are ${franchiseArr}!`);
+// }
+let franchiseArr = ['Star Wars', 'Marvel', 'DC', 'Dragonball Z', 'Game of Thrones', 'Harry Potter', 'Lord of the Rings', 'Pokemon', 'Final Fantasy', 'Star Trek'];
+let correctAnswer = false;
+let guessNumSeven = 6;
+
+for (let i = 0; i < guessNumSeven; i++) {
+  let userNumSevenGuess = prompt('Guess one of my favorite cinematic universes?').toLowerCase();
+  // guessNumSeven-=1;
+  console.log('NumberSevenGuess >>> ', userNumSevenGuess);
+  for (let j = 0; j < franchiseArr.length; j++) {
+    if (userNumSevenGuess === franchiseArr[j].toLowerCase()) {
+      alert('ONE OF MY FAVORITES!');
+      // eslint-disable-next-line no-unused-vars
+      correctAnswer = true;
+      score++;
+    }
+  }
+  if(correctAnswer === true) {
+    break;
+  }
+}
+// for (let j = 0; j < franchiseArr.length; j++) {
+//   if (franchiseArr[j].toLowerCase() !== userNumSevenGuess); {
+//     alert('Not quite - try again!');
+//   }
+//
+//   else if (userNumSevenGuess !== franchiseArr);
+//     alert('Not quite - try again!');
+
 alert(`You must be the very BEST teammate, at trivia night(s), ${visitorName}!`);
 
-alert(`Btw, you scored a total of ${score} on the earlier quiz questions. Nice Job!`);
+alert(`Psst - ${visitorName}! My favorite cinematic universes are: Star Wars, Marvel, DC, Dragonball Z, Game of Thrones, Harry Potter, Lord of the Rings, Pokemon, Final Fantasy, and Star Trek.`);
+
+alert(`${visitorName} scored a total of ${score} on the earlier quiz questions. Nice Job!`);
